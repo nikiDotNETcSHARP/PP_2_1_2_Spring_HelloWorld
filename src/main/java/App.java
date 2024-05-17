b43d5caac18ff47000d0c6138f047b318df163fa
Nikita Noskov <noskovnikita@lenta.ru>
2024-05-17 08:21:53 +0300

    HomeWork "2.1.2/2.1.3"

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,15 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld bean1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(bean1.equals(bean2));
+        System.out.println(catBean1.equals(catBean2));
     }
 }
